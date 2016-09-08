ed117dd93ea62d0f5e05c6c4d72db33efa1db135
Roland G. Barro, Jr <rolandbarro@gmail.com>
2016-09-09 07:29:47 +0800

    InitialCommit
    Just barely started

---
+++ TKDMainApp/TestConsole/DAccess.cs
@@ -0,0 +1,15 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class DAccess : Athlete
+    {
+        public string Level { get; set; }
+        public string Category { get; set; }
+    }
+}
